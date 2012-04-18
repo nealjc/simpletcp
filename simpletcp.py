09d4bd8109f8b33075e542ae2baae7a6c88e12a0
Neal <ncharbonneau@gmail.com>
2012-04-17 22:19:18 -0400

    added info for why client disconnected. removed error callback. added timeout for client. added connection re-try for client

--- simpletcp.py
+++ simpletcp.py
@@ -1,16 +1,27 @@
 """
 simpletcp is a small library that simplifies writing networking code.
-It is not as robust nor does it have as many features as a framework
-like Twisted.
+Meant for use to get a prototype up and running quickly. Definitely
+not production-level code (see something like Twisted).
 
 The library does the following:
+  - Turns the TCP socket interface into one method call for client and server
+  - Handles basic network issues: short reads/writes, disconnects
+  - Turns TCP into a message oriented stream rather than byte stream (adds
+    length headers for you)
 
+The server is a threaded TCP server (one thread per request) and everything
+is handled via callbacks (new clients, client messages, etc).
+
+The client blocking. 
 """
 import SocketServer
 import socket
 import zlib
 import threading
 import struct
+
+PROTOCOL_ERROR = 0
+CONNECTION_CLOSED = 1
 
 class SimpleTCPServerHandler(SocketServer.BaseRequestHandler):
 
@@ -20,9 +31,10 @@ class SimpleTCPServerHandler(SocketServer.BaseRequestHandler):
 
     def finish(self):
         if self.server._closed_cb:
-            self.server._closed_cb(self.client_address)
+            self.server._closed_cb(self.client_address, self._reason)
 
     def handle(self):
+        self._reason = CONNECTION_CLOSED
         while True:
             #read header for length
             try:
@@ -35,6 +47,7 @@ class SimpleTCPServerHandler(SocketServer.BaseRequestHandler):
                 data = self.read_all(length)
             except:
                 #client shouldn't have closed here. error handler
+                self._reason = PROTOCOL_ERROR
                 break
             #callback
             if self.server._decompress_func:
@@ -42,6 +55,8 @@ class SimpleTCPServerHandler(SocketServer.BaseRequestHandler):
             self.server._call_back(self.client_address, data)
 
     def read_all(self, length):
+        #TODO: don't do exception here. e.g. above
+        #client closing normally shouldn't be try/except
         remaining = length
         msg = ""
         while remaining > 0:
@@ -57,35 +72,64 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     daemon_threads = True
 
     def __init__(self, addr, handler, call_back, new_conn_cb, conn_closed_cb,
-                 err_back, decompress_func):
+                 decompress_func):
         SocketServer.TCPServer.__init__(self, addr, handler)
         self._call_back = call_back
         self._new_cb = new_conn_cb
         self._closed_cb = conn_closed_cb
         self._decompress_func = decompress_func
 
-#could try re-connecting in send_msg if the connection dies
-#still raise an excepiton, but on next call try to re-connect
-#so the object is still valid
+class NoConnection(Exception): pass
+class ConnectionTerimated(Exception): pass
+
 class TCPSender(object):
 
-    def __init__(self, dest_addr, port, compress_func, timeout):
+    def __init__(self, dest_addr, port, compress_func, timeout=5):
+        """Creates a new TCPSender.
+
+        timeout should be >= 1
+        """
+        self._compress_func = compress_func
         self._socket = socket.socket(socket.AF_INET,
                                      socket.SOCK_STREAM)
-        self._socket.connect((dest_addr, port))
-        self._compress_func = compress_func
+        self._socket.settimeout(timeout)
+        self._host = dest_addr
+        self._port = int(port)
+        self._timeout = timeout
+        self._connected = False
+
+    def try_connecting(self):
+        try:
+            self._socket.connect((self._host, self._port))
+            self._connected = True
+        except socket.error as e:
+            raise Exception("Unable to connect to your server {0}:{1} ({2})".format(
+                self._host, self._port, e))
 
     def send_msg(self, msg):
+        """Send a message to the simpletcp server. This method is blocking.
+        If the method returns normally, the message was succesfully sent.
+
+        Raises NoConnection if a connection cannot be established
+        Raises ConnectionTerminated if an error occurred while writing the data
+        """
+        if not self._connected:
+            self.try_connecting()
         if self._compress_func:
             msg = self._compress_func(msg)
         msg_len = len(msg)
         try:
             self.write_all(struct.pack("!i", msg_len))
             self.write_all(msg)
-        except:
-            print "Error"
+        except Exception as e:
+            self._connected = False
+            raise ConnectionTerimated(
+                "Error writing to simpletcp server {0}".format(e))
 
     def close(self):
+        """Close the connection to the server. Once this method is called,
+        this object can no longer be used. 
+        """
         self._socket.close()
 
     def write_all(self, msg):
@@ -100,37 +144,24 @@ class TCPSender(object):
             msg = msg[sent:]
 
 def create_server(listen_port, new_msg_cb, new_conn=None, conn_closed=None,
-                  err=None, decompress_func=zlib.decompress):
+                  decompress_func=zlib.decompress, same_thread=False):
     """Creates and starts a server listening on port listen_port. When
     a new message is (fully) received, new_msg_cb will be called
     with the message
     """
     server = ThreadedTCPServer(('localhost', listen_port),
                                SimpleTCPServerHandler,
-                               new_msg_cb, new_conn, conn_closed, err,
+                               new_msg_cb, new_conn, conn_closed,
                                decompress_func)
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.setDaemon(True)
-    server_thread.start()
+    if not same_thread:
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.setDaemon(True)
+        server_thread.start()
+    else:
+        server.serve_forever()
 
 def create_client(dest_addr, port, compress_func=zlib.compress,
                   timeout=5):
     sender = TCPSender(dest_addr, port, compress_func, timeout)
     return sender
 
-#make this include client address?
-def msg_test(cli_addr, msg):
-    print cli_addr, msg
-    
-if __name__ == '__main__':
-    import time
-    create_server(8080, msg_test)
-
-    time.sleep(1)
-    cli = create_client('localhost', 8080)
-    while True:
-        cli.send_msg("testing")
-        try:
-            time.sleep(2)
-        except:
-            break
